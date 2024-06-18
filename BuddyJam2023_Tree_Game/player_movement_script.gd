4348d9f76623db08d334b73bf86b7f5a49dcc39d
David L. Carroll <joefox67@hotmail.com>
2024-06-17 20:48:51 -0700

    First Commit
    Set up the following
       Global Variables in the global script
       Player Variables in the player script
       A main menu & the scene in front of the ship
       Basic movement thats a bit wonky with diagnoals.
       camera moves with player.

---
+++ BuddyJam2023_Tree_Game/player_movement_script.gd
@@ -0,0 +1,33 @@
+extends CharacterBody2D
+
+
+@export var SPEED = 500.0
+#const JUMP_VELOCITY = -400.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+#var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+
+func _physics_process(delta):
+	# Add the gravity.
+	#if not is_on_floor():
+	#	velocity.y += gravity * delta
+
+	# Handle jump
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	
+	var vertical_direction = Input.get_axis("ui_up","ui_down")
+	var horizontal_direction = Input.get_axis("ui_left", "ui_right")
+	#diagonal in all directions
+	if vertical_direction && horizontal_direction:
+		velocity.x = horizontal_direction * SPEED
+		velocity.y = vertical_direction * SPEED
+	# Horizontal left and right direction
+	elif horizontal_direction:
+		velocity.x = horizontal_direction * SPEED
+	elif vertical_direction:
+		velocity.y = vertical_direction * SPEED
+	else:
+		set_velocity(Vector2(0,0))
+	move_and_slide()
---
+++ BuddyJam2023_Tree_Game/player_script.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+var health_current = 10
+var health_max = 10
+var energy_current = 10
+var energy_max = 10
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
